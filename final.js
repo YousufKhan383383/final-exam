140a57ca2509f9d1b86d94ef46d2fa1247d3fdeb
Yousuf Khan <yousufkhan38383@gmail.com>
2022-04-22 13:36:29 +0600

    first commit

--- final.js
+++ final.js
@@ -23,22 +23,25 @@ function loadMeal (data){
      var imgLink = data.meals[0].strMealThumb;
      var cat = data.meals[0].strCategory;
      var title = data.meals[0].strMeal;
+     var Instruction = data.meals[0].strInstructions 
 
 
      container.innerHTML = `<p><b> ${title} </b></p>
                          <img src= "${data.meals[0].strMealThumb}">
-                         <p> Category: ${cat} </p>
+                         <p> <b> Category:</b> ${cat} </p>
+                         <p> <b> Instructions: </b> ${Instruction}</p>
                          <button onclick="showAll()"> Show All </button>`;
                          
 
  function showAll (text) {
      fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${text}`)
      .then(res=> res.json() )
-     .then(data=> loadMeal(data));
+     .then(data=> showAll(data));
  }
 
+ connectMeal();
 
-function showAll() {
+function showAll (data) {
     var newContainer = document.getElementById("new-container");
 
     newContainer.innerHTML = `<p><b> ${title} </b></p>
@@ -47,6 +50,8 @@ function showAll() {
 }
 
 
+
     container.appendChild(newDiv);
 
 }
+
